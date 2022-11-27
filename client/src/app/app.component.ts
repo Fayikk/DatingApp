453cbe5ae709b66592bfc2ecd41103c161e08e8f
Fayik <veznedaroglufayik2@gmail.com>
2022-11-27 23:05:53 +0300

    Update database,Login,Register,parent/child include implemented

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component,OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 
 
@@ -11,17 +12,19 @@ import { Component,OnInit } from '@angular/core';
 export class AppComponent implements  OnInit {
   
   title="Dating App";
-  users:any;
-constructor(private client:HttpClient){}
+  users:any ;
+constructor(private accountService : AccountService){}
 
   ngOnInit(): void {
-    this.client.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request has completed")
-    });
+    this.setCurrentUser();
   }
 
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 
 
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
